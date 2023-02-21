80101654a4c7a3557917d26161c1e82e32d3750d
nenikitov <nenikitov@mail.ru>
2023-02-21 15:43:07 -0500

    Made fetch instructions

--- chip_8/src/processor.rs
+++ chip_8/src/processor.rs
@@ -5,14 +5,27 @@ pub struct Processor {
     pub memory: Memory,
 }
 
-struct SplitBytes {
-    i: u8,
-    x: u8,
-    y: u8,
-    n: u8,
-    nn: u8,
+struct Opcode {
+    i: usize,
+    x: usize,
+    y: usize,
+    n: usize,
+    nn: u16,
     nnn: u16,
 }
+impl Opcode {
+    pub fn to_tuple(&self) -> (usize, usize, usize, usize, u16, u16) {
+        (
+            self.i,
+            self.x,
+            self.y,
+            self.n,
+            self.nn,
+            self.nnn
+        )
+    }
+}
+
 
 impl Processor {
     pub fn new() -> Self {
@@ -26,9 +39,18 @@ impl Processor {
     }
 
     pub fn fetch(&mut self) -> Instruction {
-        let instruction_bytes = self.read_instruction_bytes();
-        let instruction_bytes = Self::split_instruction_bytes(instruction_bytes);
-        todo!()
+        let opcode = self.read_instruction_bytes();
+        let opcode = Self::split_instruction_bytes(opcode);
+        let (i, x, y, n, nn, nnn) = opcode.to_tuple();
+        match (i, x, y, n) {
+            (0x0, 0x0, 0xE, 0x0) => Instruction::ClearScreen,
+            (0x1, _,   _,   _  ) => Instruction::Jump { address: nnn },
+            (0x6, _,   _,   _  ) => Instruction::RegisterSet { register: x, value: nn },
+            (0x7, _,   _,   _  ) => Instruction::RegisterAdd { register: x, value: nn },
+            (0xA, _,   _,   _  ) => Instruction::IndexSet { value: nnn },
+            (0xD, _,   _,   _  ) => Instruction::Display { register_x: x, register_y: y, height: n },
+            _ => unreachable!()
+        }
     }
 
     fn read_instruction_bytes(&mut self) -> u16 {
@@ -41,15 +63,15 @@ impl Processor {
         ((byte_1 as u16) << 8) | (byte_2 as u16)
     }
 
-    fn split_instruction_bytes(bytes: u16) -> SplitBytes {
-        let i = ((bytes & 0xF000) >> 12) as u8;
-        let x = ((bytes & 0x0F00) >> 8) as u8;
-        let y = ((bytes & 0x00F0) >> 4) as u8;
-        let n = (bytes & 0x000F) as u8;
-        let nn = (bytes & 0x00FF) as u8;
-        let nnn = bytes & 0x0FFF;
+    fn split_instruction_bytes(bytes: u16) -> Opcode {
+        let i = ((bytes & 0xF000) >> 12) as usize;
+        let x = ((bytes & 0x0F00) >> 8) as usize;
+        let y = ((bytes & 0x00F0) >> 4) as usize;
+        let n = (bytes & 0x000F) as usize;
+        let nn = (bytes & 0x00FF) as u16;
+        let nnn = (bytes & 0x0FFF) as u16;
 
-        SplitBytes { i, x, y, n, nn, nnn }
+        Opcode { i, x, y, n, nn, nnn }
     }
 }
 
